627f8adadae7eaca9754aeda92f68754d312aada
이승준 <nate199458@gmail.com>
2024-08-24 12:58:07 +0900

    feat: Applayout - max,min width

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -2,11 +2,28 @@ import { render, screen } from '@testing-library/react'
 import Layout, { linkList } from './Layout'
 
 describe('Layout', () => {
+  it('renders children correctly', () => {
+    render(
+      <Layout>
+        <div data-testid="child">Child Component</div>
+      </Layout>,
+    )
+    expect(screen.getByTestId('child')).toBeInTheDocument()
+  })
+
   it('하단 GNB가 LinkList 배열에 설정된 이름과 href 속성을 가진 Link 태그를 출력해야 한다.', () => {
     render(<Layout>children</Layout>)
 
     linkList.forEach((link) => {
       expect(screen.getByRole('link', { name: link.text })).toHaveAttribute('href', link.href)
     })
+  })
+
+  it('has correct max-width/min-width class ', () => {
+    const { container } = render(<Layout>children</Layout>)
+    const mainElement = container.querySelector('main')
+
+    expect(mainElement).toHaveClass('max-w-[480px]')
+    expect(mainElement).toHaveClass('min-w-[280px]')
   })
 })
