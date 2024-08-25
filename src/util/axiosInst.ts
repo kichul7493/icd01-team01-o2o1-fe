733e6b6e22d7f6931d02705d6d62bba468464f76
silveriron <kichul7493@gmail.com>
2024-08-25 22:14:51 +0900

    fix: axios interceptor 설정 삭제 (추후 협의하여 다시 추가)

--- src/util/axiosInst.ts
+++ src/util/axiosInst.ts
@@ -1,22 +1,8 @@
 import { baseURL } from '@/constants/api'
 import axios from 'axios'
 
-const AUTHORIZATION_TOKEN = 'Authorization'
-const REFRESH_TOKEN = 'RefreshAuth'
-
 const axiosInst = axios.create({
   baseURL: baseURL,
 })
-
-axiosInst.interceptors.request.use((config) => {
-  const token = window.localStorage.getItem(AUTHORIZATION_TOKEN)
-  const refreshToken = window.localStorage.getItem(REFRESH_TOKEN)
-  if (token && token !== 'null' && refreshToken && refreshToken !== 'null') {
-    config.headers.Authorization = `${token}`
-    config.headers.RefreshAuth = `${refreshToken}`
-  }
-
-  return config
-}, null)
 
 export default axiosInst
