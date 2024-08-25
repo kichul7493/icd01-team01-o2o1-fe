3501240ae93866f2fe08035eb3de83b4de1b80bf
이승준 <nate199458@gmail.com>
2024-08-26 01:32:09 +0900

    feat: gnb 렌더링 조건 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image'
 
-export default function Home() {
-  return <div className="flex h-full w-full"></div>
+export default function LoginPage() {
+  return (
+    <div className="flex h-full w-full">
+      <div className="flex items-center justify-center">
+        <span className="text-2xl">배달이써</span>
+      </div>
+    </div>
+  )
 }
