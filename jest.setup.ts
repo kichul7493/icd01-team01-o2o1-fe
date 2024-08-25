07bfd2fca3f90f5c847f42ffeeef2c3484be16c0
silveriron <kichul7493@gmail.com>
2024-08-25 22:13:07 +0900

    fix: jest setup에서 msw import 수정

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,5 @@
+import { server } from '@/mocks/node'
 import '@testing-library/jest-dom'
-import { server } from './src/mocks/server'
 
 // msw를 이용한 서버를 일단 띄워서 listen 해준다.
 beforeAll(() => server.listen())
