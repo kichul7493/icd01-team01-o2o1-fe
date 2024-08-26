714b0684e412d40c4128cfd3c0d054fa77b60516
silveriron <kichul7493@gmail.com>
2024-08-26 20:58:46 +0900

    feat: 리뷰 등록 폼

---
+++ src/components/review/ReviewForm.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import ImageUploader from '../AddReview/ImageUploader'
+import Rating from '../AddReview/Rating'
+
+const ReviewForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('submit')
+  }
+
+  return (
+    <form className="px-10" onSubmit={handleSubmit}>
+      <p className="mb-4 font-semibold">삼청당 고대안암점</p>
+      <Rating />
+      <textarea
+        className="mb-4 h-40 w-full rounded border border-gray-300 p-4"
+        placeholder="리뷰를 작성해주세요."
+      ></textarea>
+      <ImageUploader />
+      <button
+        className="absolute bottom-0 left-0 z-[100] h-24 w-full bg-main text-xl font-semibold text-white"
+        type="submit"
+      >
+        등록하기
+      </button>
+    </form>
+  )
+}
+
+export default ReviewForm
