7cf5c3c583cdb4702314f0b0371227397ab748e3
sionyeom <duatldhs5555@gmail.com>
2024-08-28 19:48:53 +0900

    Fix: props interface 수정

--- src/page/StoreDetailInfoScreen.tsx
+++ src/page/StoreDetailInfoScreen.tsx
@@ -1,6 +1,6 @@
 import BackBtn from '@/components/storeDetailInfo/BackBtn'
 import LocationMap from '@/components/storeDetailInfo/LocationMap'
-import Info from '@/components/storeDetailInfo/Info'
+import StoreInfo from '@/components/storeDetailInfo/StoreDescribe'
 import Thumbnail from '@/components/storeDetailInfo/Thumbnail'
 
 const StoreDetailInfoScreen = () => {
@@ -9,12 +9,12 @@ const StoreDetailInfoScreen = () => {
       <BackBtn storeTitle={'후라이드 참 잘하는 집'} />
       <LocationMap />
       <main className="px-3 pt-5">
-        <Info
+        <StoreInfo
           address={'서울 서초구 반포대로 19길 10 3층'}
           phone={'02-0000-0000'}
           openTime={'11:00'}
           closeTime={'23:00'}
-          location={'배달 가능 지역'}
+          location={true}
         />
       </main>
       <main className="mb-[153px] mt-[34px]">
