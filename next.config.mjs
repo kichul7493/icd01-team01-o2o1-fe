86262684e9da3bcca1b7cf8d8847018cfcdddd52
이승준 <nate199458@gmail.com>
2024-08-29 18:14:14 +0900

    feat: svg 컴포넌트 webpack

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['via.placeholder.com'],
   },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
 
 export default nextConfig
