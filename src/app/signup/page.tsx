9ec88845905d692023e655fa8cb9ade780050bbb
이승준 <nate199458@gmail.com>
2024-08-29 17:02:59 +0900

    feat: 공통 컴포넌트

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/shared/Header/indes'
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Header />
+      SignupPage
+    </div>
+  )
+}
+
+export default SignupPage
---
+++ src/components/shared/Header/indes.tsx
@@ -0,0 +1,24 @@
+// src/Header.tsx
+import React from 'react'
+
+export interface HeaderProps {
+  left?: React.ReactNode
+  center?: React.ReactNode
+  right?: React.ReactNode
+  className?: string
+  position?: 'static' | 'fixed' | 'absolute'
+}
+
+const Header = ({ left, center, right, position = 'static', ...props }: HeaderProps) => {
+  return (
+    <header
+      className={`flex h-12 items-center justify-between px-4 ${position} w-full max-w-[480px] ${props.className} `}
+    >
+      <div className="w-1/3">{left || <div />}???</div>
+      <div className="flex w-1/3 items-center justify-center">{center || <div />}!!</div>
+      <div className="flex w-1/3 items-center justify-end">{right || <div />}!!!</div>
+    </header>
+  )
+}
+
+export default Header
