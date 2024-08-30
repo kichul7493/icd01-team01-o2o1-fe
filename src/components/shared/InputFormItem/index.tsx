bca7d1a3b805eed04d7f0b0a6a09127b08b2c388
이승준 <nate199458@gmail.com>
2024-08-30 16:08:04 +0900

    feat: form & input component 추가

---
+++ src/components/shared/InputFormItem/index.tsx
@@ -0,0 +1,43 @@
+import { FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import React from 'react'
+import { Control } from 'react-hook-form'
+
+type InputFormItemProps = {
+  control: Control<any, any>
+  name: string
+  label?: string | React.ReactNode
+  placeholder: string
+  required?: boolean
+}
+
+const InputFormItem = ({ required = false, ...props }: InputFormItemProps) => {
+  return (
+    <FormField
+      control={props.control}
+      name={props.name}
+      rules={{ required: required }}
+      render={({ field }) => {
+        return (
+          <FormItem>
+            <FormLabel>
+              <div>
+                {props.label}
+                {required && <span className="text-red-500"> *</span>}
+              </div>
+            </FormLabel>
+            <FormControl>
+              <Input
+                className="h-[50px] bg-gray-50 pl-[14px]"
+                placeholder={props.placeholder}
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )
+      }}
+    />
+  )
+}
+
+export default InputFormItem
