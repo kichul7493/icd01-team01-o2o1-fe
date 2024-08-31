11a8732443d4412c4161483a3a39837fe3ea0151
silveriron <kichul7493@gmail.com>
2024-08-31 12:30:47 +0900

    test: test ID 변경에 따라 테스트 코드 수정

--- src/components/AddReview/Rating.test.tsx
+++ src/components/AddReview/Rating.test.tsx
@@ -7,32 +7,29 @@ describe('Rating', () => {
     render(<Rating />)
 
     const buttons = screen.getAllByRole('button')
-    const stars = screen.getAllByTestId('starIcon')
 
     expect(buttons).toHaveLength(5)
-    expect(stars).toHaveLength(5)
   })
 
   it('버튼을 클릭하면 등급이 변경된다. 변경된 등급만큼 버튼(별)의 색상이 변경된다.', async () => {
     render(<Rating />)
 
     const buttons = screen.getAllByRole('button')
-    const stars = screen.getAllByTestId('starIcon')
 
     await userEvent.click(buttons[2])
 
-    expect(stars[0]).toHaveClass('text-yellow-400')
-    expect(stars[1]).toHaveClass('text-yellow-400')
-    expect(stars[2]).toHaveClass('text-yellow-400')
-    expect(stars[3]).toHaveClass('text-gray-300')
-    expect(stars[4]).toHaveClass('text-gray-300')
+    expect(screen.getByTestId('starIcon-0')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-1')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-2')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-3')).toHaveClass('text-gray-400')
+    expect(screen.getByTestId('starIcon-4')).toHaveClass('text-gray-400')
 
     await userEvent.click(buttons[4])
 
-    expect(stars[0]).toHaveClass('text-yellow-400')
-    expect(stars[1]).toHaveClass('text-yellow-400')
-    expect(stars[2]).toHaveClass('text-yellow-400')
-    expect(stars[3]).toHaveClass('text-yellow-400')
-    expect(stars[4]).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-0')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-1')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-2')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-3')).toHaveClass('text-yellow-400')
+    expect(screen.getByTestId('starIcon-4')).toHaveClass('text-yellow-400')
   })
 })
