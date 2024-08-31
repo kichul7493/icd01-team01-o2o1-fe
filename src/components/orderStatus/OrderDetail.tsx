ab9e584feb9ad7ce757d508d4a68ab928aac2ac2
silveriron <kichul7493@gmail.com>
2024-08-31 10:17:50 +0900

    refactor: 주문 메뉴 목록 분리

--- src/components/orderStatus/OrderDetail.tsx
+++ src/components/orderStatus/OrderDetail.tsx
@@ -1,5 +1,6 @@
 import { Menu } from '@/features/orderStatus/types'
 import React from 'react'
+import OrderMenuItem from './OrderMenuItem'
 
 interface OrderDetailProps {
   orderId: number
@@ -14,21 +15,7 @@ const OrderDetail = ({ orderId, storeName, menuList, totalPrice }: OrderDetailPr
       <p className="text-xl font-semibold">{orderId} 주문</p>
       <p>{storeName}</p>
       {menuList.map((menu) => (
-        <div key={menu.menuId} className="mb-3 flex gap-3">
-          <div className="flex h-7 w-7 items-center justify-center rounded-full bg-gray-300">
-            {menu.menuCount}
-          </div>
-          <div className="flex flex-col gap-3">
-            <p className="text-sm">{menu.menuName}</p>
-            {menu.optionGroup.map((option) => (
-              <div key={option.optionGroupId} className="flex gap-2 text-sm">
-                {option.option.map((optionItem) => (
-                  <p key={optionItem.optionId}>{optionItem.optionName}</p>
-                ))}
-              </div>
-            ))}
-          </div>
-        </div>
+        <OrderMenuItem key={menu.menuId} menu={menu} />
       ))}
 
       <div>
