c65b388fa8f5667a14e19a96c02fc7614f303654
silveriron <kichul7493@gmail.com>
2024-08-31 21:50:07 +0900

    test: 리뷰 작성 테스트

--- src/components/AddReview/Rating.test.tsx
+++ src/components/AddReview/Rating.test.tsx
@@ -3,8 +3,10 @@ import Rating from './Rating'
 import userEvent from '@testing-library/user-event'
 
 describe('Rating', () => {
+  const handleChange = jest.fn()
+
   it('5개의 버튼(별)이 출력된다.', () => {
-    render(<Rating />)
+    render(<Rating value={0} handleChange={handleChange} />)
 
     const buttons = screen.getAllByRole('button')
 
@@ -12,24 +14,12 @@ describe('Rating', () => {
   })
 
   it('버튼을 클릭하면 등급이 변경된다. 변경된 등급만큼 버튼(별)의 색상이 변경된다.', async () => {
-    render(<Rating />)
+    render(<Rating value={0} handleChange={handleChange} />)
 
     const buttons = screen.getAllByRole('button')
 
     await userEvent.click(buttons[2])
 
-    expect(screen.getByTestId('starIcon-0')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-1')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-2')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-3')).toHaveClass('text-gray-400')
-    expect(screen.getByTestId('starIcon-4')).toHaveClass('text-gray-400')
-
-    await userEvent.click(buttons[4])
-
-    expect(screen.getByTestId('starIcon-0')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-1')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-2')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-3')).toHaveClass('text-yellow-400')
-    expect(screen.getByTestId('starIcon-4')).toHaveClass('text-yellow-400')
+    expect(handleChange).toHaveBeenCalledWith(3)
   })
 })
