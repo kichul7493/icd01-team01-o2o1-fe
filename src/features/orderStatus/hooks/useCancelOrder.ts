7ebb36d08ac9d407c32ccc4180e7da32075af4de
silveriron <kichul7493@gmail.com>
2024-09-02 19:36:05 +0900

    fix: 주문 취소 에러 핸들링

--- src/features/orderStatus/hooks/useCancelOrder.ts
+++ src/features/orderStatus/hooks/useCancelOrder.ts
@@ -12,6 +12,9 @@ export const useCancelOrder = () => {
       return axiosInst.delete(`/order/${orderId}`)
     },
     onSuccess: () => router.back(),
+    onError: () => {
+      alert('에러가 발생했습니다. 잠시 후 다시 시도해주세요.')
+    },
   })
 
   const handleCancelOrder = () => {
