fe1fb6b3a9fddbd822df3c157e94f034b6861310
이승준 <nate199458@gmail.com>
2024-09-04 10:26:25 +0900

    feat: 컴포넌트 추가 + my page

---
+++ src/app/my/_components/MyPageMenu.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MyPageMenu = () => {
+  return <div>MyPageMenu</div>
+}
+
+export default MyPageMenu
---
+++ src/app/my/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyPage = () => {
+  return (
+    <>
+      <UserInfo />
+      <MyPageMenu />
+    </>
+  )
+}
+
+export default MyPage
