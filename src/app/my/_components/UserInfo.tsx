61019503416246cdeca63521b5f3757da5883fb7
이승준 <nate199458@gmail.com>
2024-09-07 13:02:06 +0900

    feat: mock api - query hook 작성

--- src/app/my/_components/UserInfo.tsx
+++ src/app/my/_components/UserInfo.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { useGetMemberHooks } from '@/features/member/hooks/useGetMemberHook'
 import React from 'react'
 
 const UserInfo = () => {
+  const { data, isLoading } = useGetMemberHooks()
+  if (isLoading) return <div>로딩중...</div>
+
   return (
     <div>
-      <div className="text-xl">{'test'}</div>
+      <div className="text-xl">{data!.nickName}</div>
       <div>{'010-0000-0000'}</div>
     </div>
   )
--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,6 @@
 import { authHandlers } from './auth'
+import { memberHandlers } from './member'
 import { orderHandlers } from './order'
 import { reviewHandlers } from './review'
 
-export const handlers = [...reviewHandlers, ...orderHandlers, ...authHandlers]
+export const handlers = [...reviewHandlers, ...orderHandlers, ...authHandlers, ...memberHandlers]
