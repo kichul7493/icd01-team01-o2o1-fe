9b81068d38ac37e21f9e4ba75985fb89e216d9e1
sionyeom <duatldhs5555@gmail.com>
2024-09-11 00:12:22 +0900

    feat: 음식점 매장 정보 기능 작성

--- src/app/store/[id]/detail/_components/Thumbnail.tsx
+++ src/app/store/[id]/detail/_components/Thumbnail.tsx
@@ -1,13 +1,18 @@
+'use client'
+
 import Image from 'next/image'
+import { useGetStoreDetailInfo } from '@/features/store/hooks/useGetStoreDetailInfo'
 
 interface ThumbnailProps {
   image: string
 }
 
 const Thumbnail = ({ image }: ThumbnailProps) => {
+  const { data, isLoading } = useGetStoreDetailInfo()
+
   return (
     <figure className="relative h-[220px] w-full bg-gray-200">
-      <Image src={image} alt={`Thumbnail`} fill />
+      {data?.thumbnails[0] ? <Image src={data?.thumbnails[0]} alt={`Thumbnail`} fill /> : <></>}
     </figure>
   )
 }
