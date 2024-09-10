6e9d2c727b01f7eaea5b59f08c9827139898140a
sionyeom <duatldhs5555@gmail.com>
2024-09-10 22:12:18 +0900

    Feat: mock 데이터 추가

--- src/mocks/handlers/store.ts
+++ src/mocks/handlers/store.ts
@@ -20,14 +20,136 @@ export const storeHandlers = [
         minimumPrice: 15000,
         reviewCount: 999,
         reviewRate: 4.5,
-        thumbnails: [],
+        thumbnails: [
+          'https://via.placeholder.com/375x220?text=Image+1',
+          'https://via.placeholder.com/375x220?text=Image+2',
+        ],
         menus: [
           {
             menuId: 101,
             menuName: '비빔밥',
             description: '신선한 야채와 고소한 참기름이 어우러진 전통 비빔밥',
             menuPrice: 8000,
-            menuImages: [],
+            menuImages: [
+              'https://via.placeholder.com/100x100?text=Image+1',
+              'https://via.placeholder.com/100x100?text=Image+2',
+            ],
+            optionGroups: [
+              {
+                optionGroupId: 201,
+                optionGroupName: '추가 토핑',
+                isRequired: true,
+                options: [
+                  {
+                    optionId: 301,
+                    optionName: '계란 추가',
+                    optionPrice: 1000,
+                  },
+                  {
+                    optionId: 302,
+                    optionName: '고기 추가',
+                    optionPrice: 2000,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            menuId: 102,
+            menuName: '산채비빔밥',
+            description: '신선한 야채와 고소한 참기름이 어우러진 전통 비빔밥',
+            menuPrice: 9000,
+            menuImages: [
+              'https://via.placeholder.com/100x100?text=Image+1',
+              'https://via.placeholder.com/100x100?text=Image+2',
+            ],
+            optionGroups: [
+              {
+                optionGroupId: 201,
+                optionGroupName: '추가 토핑',
+                isRequired: true,
+                options: [
+                  {
+                    optionId: 301,
+                    optionName: '계란 추가',
+                    optionPrice: 1000,
+                  },
+                  {
+                    optionId: 302,
+                    optionName: '고기 추가',
+                    optionPrice: 2000,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            menuId: 103,
+            menuName: '소고기비빔밥',
+            description: '신선한 야채와 고소한 참기름이 어우러진 전통 비빔밥',
+            menuPrice: 11000,
+            menuImages: [
+              'https://via.placeholder.com/100x100?text=Image+1',
+              'https://via.placeholder.com/100x100?text=Image+2',
+            ],
+            optionGroups: [
+              {
+                optionGroupId: 201,
+                optionGroupName: '추가 토핑',
+                isRequired: true,
+                options: [
+                  {
+                    optionId: 301,
+                    optionName: '계란 추가',
+                    optionPrice: 1000,
+                  },
+                  {
+                    optionId: 302,
+                    optionName: '고기 추가',
+                    optionPrice: 2000,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            menuId: 104,
+            menuName: '고기냉면',
+            description: '신선한 야채와 고소한 참기름이 어우러진 전통 비빔밥',
+            menuPrice: 9000,
+            menuImages: [
+              'https://via.placeholder.com/100x100?text=Image+1',
+              'https://via.placeholder.com/100x100?text=Image+2',
+            ],
+            optionGroups: [
+              {
+                optionGroupId: 201,
+                optionGroupName: '추가 토핑',
+                isRequired: true,
+                options: [
+                  {
+                    optionId: 301,
+                    optionName: '계란 추가',
+                    optionPrice: 1000,
+                  },
+                  {
+                    optionId: 302,
+                    optionName: '고기 추가',
+                    optionPrice: 2000,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            menuId: 105,
+            menuName: '비빔냉면',
+            description: '신선한 야채와 고소한 참기름이 어우러진 전통 비빔밥',
+            menuPrice: 9000,
+            menuImages: [
+              'https://via.placeholder.com/100x100?text=Image+1',
+              'https://via.placeholder.com/100x100?text=Image+2',
+            ],
             optionGroups: [
               {
                 optionGroupId: 201,
