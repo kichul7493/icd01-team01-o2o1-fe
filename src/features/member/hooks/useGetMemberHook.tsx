76962775cea7be90fda556f3ec3a094fbafee619
silveriron <kichul7493@gmail.com>
2024-10-19 10:12:27 +0900

    feat: 사용자 정보 페이지 예외 처리

--- src/features/member/hooks/useGetMemberHook.tsx
+++ src/features/member/hooks/useGetMemberHook.tsx
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query'
 import { getMemberInfo } from '../api'
 
 export const useGetMemberHooks = () => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError, refetch } = useQuery({
     queryKey: ['member'],
     queryFn: getMemberInfo,
   })
-  return { data, isLoading }
+  return { data, isLoading, isError, refetch }
 }
